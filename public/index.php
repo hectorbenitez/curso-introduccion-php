36a9e2cd90f89e811a4bba7463c80141ff0702b1
Hector Benitez <hector.benitez.fletes@gmail.com>
2018-08-14 16:56:44 -0500

    front controller

---
+++ public/index.php
@@ -0,0 +1,35 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_starup_error', 1);
+error_reporting(E_ALL);
+
+require_once '../vendor/autoload.php';
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'cursophp',
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
+
+$route = $_GET['route'] ?? '/';
+
+if ($route == '/') {
+    require '../index.php';
+} elseif ($route == 'addJob') {
+    require '../addJob.php';
+}
