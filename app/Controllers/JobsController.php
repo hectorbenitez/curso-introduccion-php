a0da43ade5a87a82a6256795d2e9214fb31edd29
Hector Benitez <hector.benitez.fletes@gmail.com>
2018-08-15 10:41:49 -0500

    validator

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -2,17 +2,33 @@
 namespace App\Controllers;
 
 use App\Models\Job;
+use Respect\Validation\Validator as v;
 
 class JobsController extends BaseController {
     public function getAddJobAction($request) {
+        $responseMessage = null;
+
         if ($request->getMethod() == 'POST') {
             $postData = $request->getParsedBody();
-            $job = new Job();
-            $job->title = $postData['title'];
-            $job->description = $postData['description'];
-            $job->save();
+            $jobValidator = v::key('title', v::stringType()->notEmpty())
+                  ->key('description', v::stringType()->notEmpty());
+
+            try {
+                $jobValidator->assert($postData);
+                $postData = $request->getParsedBody();
+                $job = new Job();
+                $job->title = $postData['title'];
+                $job->description = $postData['description'];
+                $job->save();
+
+                $responseMessage = 'Saved';
+            } catch (\Exception $e) {
+                $responseMessage = $e->getMessage();
+            }
         }
 
-        return $this->renderHTML('addJob.twig');
+        return $this->renderHTML('addJob.twig', [
+            'responseMessage' =>$responseMessage
+        ]);
     }
 }
