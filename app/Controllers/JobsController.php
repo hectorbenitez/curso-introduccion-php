fbf73344cbb1095e1d673317f1835eef6fb46b59
Hector Benitez <hector.benitez.fletes@gmail.com>
2018-08-15 11:46:30 -0500

    files

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -16,6 +16,15 @@ class JobsController extends BaseController {
             try {
                 $jobValidator->assert($postData);
                 $postData = $request->getParsedBody();
+
+                $files = $request->getUploadedFiles();
+                $logo = $files['logo'];
+
+                if($logo->getError() == UPLOAD_ERR_OK) {
+                    $fileName = $logo->getClientFilename();
+                    $logo->moveTo("uploads/$fileName");
+                }
+
                 $job = new Job();
                 $job->title = $postData['title'];
                 $job->description = $postData['description'];
