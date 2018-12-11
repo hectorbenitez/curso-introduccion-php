35b98d5800760187d18c6cf02b2125e53e043941
Hector Benitez <hector.benitez.fletes@gmail.com>
2018-12-10 22:49:02 -0600

    Adding phinx

---
+++ db/migrations/20181207053902_creating_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class CreatingUsersTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->addColumn('email', 'string')
+            ->addColumn('password', 'string')
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->create();
+    }
+}
